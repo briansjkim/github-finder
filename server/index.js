802095815bb71a739e93d68fd6744cc046261c37
Brian Kim <briaansjkim@gmail.com>
2020-06-10 15:06:41 -0400

    added bundle file to gitignore and served files to dist folder

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const PORT = 4000;
 const app = express();
+
+app.use(express.static(path.join(__dirname + '/../client/dist')));
 
 app.get('/api/repos', (req, res) => {
     res.send('GET successful');
