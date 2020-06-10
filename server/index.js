a72349cd4b93cc072359386306e6b5ae9a4347ba
Brian Kim <briaansjkim@gmail.com>
2020-06-10 14:58:53 -0400

    reverted back to node for now

--- server/index.js
+++ server/index.js
@@ -1,45 +1,15 @@
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const PORT = 4000;
-// const app = express();
-
-// app.get('/api/repos', (req, res) => {
-//     res.send('GET successful');
-// });
-
-// app.post('/api/repos', (req, res) => {
-//     res.send('POST successful');
-// })
-
-
-// app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
-
 const express = require('express');
+const bodyParser = require('body-parser');
+const PORT = 4000;
 const app = express();
-const graphqlHTTP = require('express-graphql');
-const { buildSchema } = require('graphql');
 
-const schema = buildSchema(`
-    type Query {
-        hello: String,
-        goodbye: String
-    }
-`);
+app.get('/api/repos', (req, res) => {
+    res.send('GET successful');
+});
 
-const root = {
-    hello: () => {
-        return 'Hello world';
-    },
-    goodbye: () => {
-        return 'Goodbye';
-    }
-};
+app.post('/api/repos', (req, res) => {
+    res.send('POST successful');
+})
 
-app.use('/graphql', graphqlHTTP({
-    schema: schema,
-    rootValue: root,
-    graphiql: true,
-}));
 
-app.listen(4000);
-console.log('Running GraphQL server')
+app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
