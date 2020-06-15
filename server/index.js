8c215fabbb64b38083f9ad4af44f86610254c0a3
Brian Kim <briaansjkim@gmail.com>
2020-06-15 16:31:29 -0400

    implemented axios get request and retrieved repos

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
 const db = require('../database/index.js');
+const config = require('../config/config.js');
 const path = require('path');
 const app = express();
 const PORT = 4000;
 
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname + '/../client/dist')));
 
 app.get('/api/repos', (req, res) => {
@@ -13,8 +15,17 @@ app.get('/api/repos', (req, res) => {
 });
 
 app.post('/api/repos', (req, res) => {
-    // const { username } = req.body;
-    // axios.get(`https://api.github.com/users/${username}/repos`)
+    const { username } = req.body;
+    axios.get(`https://api.github.com/users/${username}/repos`, {
+        'Authorization': `token ${config.TOKEN}`
+    })
+    .then((result) => {
+        return result.data;
+    })
+    .then((data) => {
+        data.forEach(repo => db.saveRepo(repo));
+    })
+    .catch((error) => console.log(error));
 });
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
