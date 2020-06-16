d037936e1a6e9151053fdbe56254d8e66bcb1127
Brian Kim <briaansjkim@gmail.com>
2020-06-16 17:07:58 -0400

    added more styling

--- client/src/components/Users.jsx
+++ client/src/components/Users.jsx
@@ -5,7 +5,7 @@ const Users = ({ repo }) => {
     return (
         <div className={styles.repo}>
             <h1>{repo.owner_login}</h1>
-            <h3><a href={repo.repos_url}>{repo.name}</a></h3>
+            <h3><a target="_blank" href={repo.repos_url}>{repo.name}</a></h3>
             {/* <p>Github Link: <a src="">{repo.repos_url}</a></p> */}
         </div>
     )
