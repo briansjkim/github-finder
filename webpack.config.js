f44aa3a1543b3c0b5893fb6d4e22c1e24fb6af10
Brian Kim <briaansjkim@gmail.com>
2020-06-16 16:16:53 -0400

    configured webpack file in order to use css

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,34 @@
 module.exports = {
-  entry: __dirname + "/client/src/index.jsx",
+  entry: __dirname + '/client/src/index.jsx',
   module: {
     rules: [
       {
         test: /\.jsx$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-react", "@babel/preset-env"],
-          },
-        },
+            presets: ['@babel/preset-react', '@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              modules: true
+            }
+          }
+        ]
       }
-    ],
+    ]
   },
   output: {
-    filename: "bundle.js",
-    path: __dirname + "/client/dist",
-    publicPath: "/",
-  },
+    filename: 'bundle.js',
+    path: __dirname + '/client/dist'
+  }
 };
