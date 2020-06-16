8a6eff65f1ff107fdb7c362dc7752c7d29eb183b
Brian Kim <briaansjkim@gmail.com>
2020-06-16 17:00:03 -0400

    changed class name

--- client/src/components/Users.jsx
+++ client/src/components/Users.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import styles from './App.css';
 
 const Users = ({ repo }) => {
     return (
-        <div className="Repo">
+        <div className={styles.repo}>
             <p>{repo.owner_login}</p>
             <p>{repo.name}</p>
             <p>{repo.repos_url}</p>
