88a21eee6af9999184af90b0d498565562aaa7e0
Brian Kim <briaansjkim@gmail.com>
2020-06-17 17:10:27 -0400

    created copyright component

---
+++ client/src/components/Copyright.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Copyright = () => {
+    return (
+        <div>
+            <span>Copyright &copy; Brian Kim 2020</span>
+        </div>
+    )
+}
+
+export default Copyright;
