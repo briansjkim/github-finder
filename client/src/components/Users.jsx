1946a54509a42488cf1c545229d736b45bc23745
Brian Kim <briaansjkim@gmail.com>
2020-06-17 16:50:01 -0400

    removed unnecessary line of code

--- client/src/components/Users.jsx
+++ client/src/components/Users.jsx
@@ -6,7 +6,6 @@ const Users = ({ repo }) => {
         <div className={styles.repo}>
             <h1>{repo.owner_login}</h1>
             <h3><a target="_blank" href={repo.repos_url}>{repo.name}</a></h3>
-            {/* <p>Github Link: <a src="">{repo.repos_url}</a></p> */}
         </div>
     )
 }
