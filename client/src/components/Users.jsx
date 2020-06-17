350b89416f9fea0f5d9256f30df66741a717e2e5
Brian Kim <briaansjkim@gmail.com>
2020-06-17 17:54:13 -0400

    adjustments to build scripts and styling

--- client/src/components/Users.jsx
+++ client/src/components/Users.jsx
@@ -5,7 +5,7 @@ const Users = ({ repo }) => {
     return (
         <div className={styles.repo}>
             <h1>{repo.owner_login}</h1>
-            <h3><a target="_blank" href={repo.repos_url}>{repo.name}</a></h3>
+            <h3>Github Link: <a target="_blank" href={repo.repos_url}>{repo.name}</a></h3>
         </div>
     )
 }
