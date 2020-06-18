fdea3d48aa271f33415059fd3caae5d0b2c0f0e0
Brian Kim <briaansjkim@gmail.com>
2020-06-18 15:19:55 -0400

    init jest testing

--- server/server.spec.js
+++ server/server.spec.js
@@ -1,8 +1,13 @@
-// Test should fail because 1 !== 2
-it('Testing to see if Jest works', () => {
-    expect(1).toBe(2);
-});
+describe('Init Jest Test', function() {
+    // Test should fail because 1 !== 2
+    it('Testing to see if Jest works', () => {
+        expect(1).toBe(2);
+    });
+    // Test should pass because 2 === 2
+    it('Testing to see if Jest works', () => {
+        expect(2).toBe(2);
+    });
+})
 
-it('Testing to see if Jest works', () => {
-    expect(2).toBe(2);
-});
+
+
