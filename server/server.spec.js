bb07996b915299edfff3ce441a1a09f5e6eb518b
Brian Kim <briaansjkim@gmail.com>
2020-06-18 15:39:59 -0400

    set up test with endpoint

--- server/server.spec.js
+++ server/server.spec.js
@@ -4,12 +4,25 @@ const request = supertest(app);
 
 describe('Init Jest Test', function() {
     // Test should fail because 1 !== 2
-    it('Testing to see if Jest works', () => {
-        expect(1).toBe(2);
-    });
+    // it('Testing to see if Jest works', () => {
+    //     expect(1).toBe(2);
+    // });
+    
     // Test should pass because 2 === 2
     it('Testing to see if Jest works', () => {
         expect(2).toBe(2);
     });
 });
 
+app.get('/test', async (req, res) => {
+    res.json({message: 'pass'});
+});
+
+it('Gets the test endpoint', async done => {
+    // Sends GET request to /test endpoint
+    const res = await request.get('/test');
+
+    expect(res.status).toBe(200);
+    expect(res.body.message).toBe('pass');
+    done()
+})
