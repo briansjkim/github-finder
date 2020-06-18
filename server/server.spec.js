84a93857c06c13af004ab45460d45f43d798f569
Brian Kim <briaansjkim@gmail.com>
2020-06-18 16:32:27 -0400

    jest test suite

--- server/server.spec.js
+++ server/server.spec.js
@@ -1,28 +1,17 @@
 const app = require('./index.js');
 const supertest = require('supertest');
 const request = supertest(app);
-
-describe('Init Jest Test', function() {
-    // Test should not pass because 1 !== 2
-    it('Testing to see if Jest works', () => {
-        expect(1).not.toBe(2);
-    });
-
-    // Test should pass because 2 === 2
-    it('Testing to see if Jest works', () => {
-        expect(2).toBe(2);
-    });
-});
+const db = require('../database/index.js');
 
 describe('Test GET api/repos endpoint', () => {
-    it('It should response with 200', async done => {
+    it('It should respond with 200', async done => {
         const res = await request.get('/api/repos');
 
         expect(res.status).toBe(200);
         done();
     });
 
-    it('It should response with an array of only 25 repos', async done => {
+    it('It should respond with an array of only 25 repos', async done => {
         const res = await request.get('/api/repos');
         const resRepos = JSON.parse(res.text);
         
@@ -32,10 +21,10 @@ describe('Test GET api/repos endpoint', () => {
 });
 
 describe('Test POST api/repos endpoint', () => {
-    it('It should response with 201', async done => {
+    it('It should respond with 201', async done => {
         const res = await request.post('/api/repos');
 
         expect(res.status).toBe(201);
         done();
-    })
+    });
 })
