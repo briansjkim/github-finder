2d2221ffbda0a4e1e9f16d4650ab814e78993495
Brian Kim <briaansjkim@gmail.com>
2020-06-18 16:16:19 -0400

    successful test with retrieved data

--- server/server.spec.js
+++ server/server.spec.js
@@ -3,26 +3,44 @@ const supertest = require('supertest');
 const request = supertest(app);
 
 describe('Init Jest Test', function() {
-    // Test should fail because 1 !== 2
-    // it('Testing to see if Jest works', () => {
-    //     expect(1).toBe(2);
-    // });
-    
+    // Test should not pass because 1 !== 2
+    it('Testing to see if Jest works', () => {
+        expect(1).not.toBe(2);
+    });
+
     // Test should pass because 2 === 2
     it('Testing to see if Jest works', () => {
         expect(2).toBe(2);
     });
 });
 
-app.get('/test', async (req, res) => {
-    res.json({message: 'pass'});
-});
+// app.get('/api/repos', async (req, res) => {
+//     // res.json({message: 'pass'});
+//     db.getRepos(req, res);
+// });
 
-it('Gets the test endpoint', async done => {
-    // Sends GET request to /test endpoint
-    const res = await request.get('/test');
+// it('Gets the test endpoint', async done => {
+//     // Sends GET request to /test endpoint
+//     const res = await request.get('/api/repos');
 
-    expect(res.status).toBe(200);
-    expect(res.body.message).toBe('pass');
-    done()
+//     expect(res.status).toBe(200);
+//     expect(res.body.message).toBe('pass');
+//     done()
+// })
+
+describe('Test api/repos endpoint', () => {
+    it('It should response with 200', async done => {
+        const res = await request.get('/api/repos');
+
+        expect(res.status).toBe(200);
+        done();
+    });
+
+    it('It should response with an array of only 25 repos', async done => {
+        const res = await request.get('/api/repos');
+        const resRepos = JSON.parse(res.text);
+        
+        expect(resRepos.length).toBe(25)
+        done();
+    })
 })
