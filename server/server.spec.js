1db267a4d7f0f86e6d88f6217fdd508496b74cd1
Brian Kim <briaansjkim@gmail.com>
2020-06-18 15:15:19 -0400

    init testing suite to make sure jest works

---
+++ server/server.spec.js
@@ -0,0 +1,8 @@
+// Test should fail because 1 !== 2
+it('Testing to see if Jest works', () => {
+    expect(1).toBe(2);
+});
+
+it('Testing to see if Jest works', () => {
+    expect(2).toBe(2);
+});
