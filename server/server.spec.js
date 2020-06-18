09660e0fcd697a579d9013ee44d833686355c3d8
Brian Kim <briaansjkim@gmail.com>
2020-06-18 17:30:41 -0400

    added another post request test

--- server/server.spec.js
+++ server/server.spec.js
@@ -10,22 +10,22 @@ beforeAll(async () => {
     await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 });
 
-describe('Test GET api/repos endpoint', () => {
-    it('It should respond with 200', async done => {
-        const res = await request.get('/api/repos');
+// describe('Test GET api/repos endpoint', () => {
+//     it('It should respond with 200', async done => {
+//         const res = await request.get('/api/repos');
 
-        expect(res.status).toBe(200);
-        done();
-    });
+//         expect(res.status).toBe(200);
+//         done();
+//     });
 
-    it('It should respond with an array of only 25 repos', async done => {
-        const res = await request.get('/api/repos');
-        const resRepos = JSON.parse(res.text);
+//     it('It should respond with an array of only 25 repos', async done => {
+//         const res = await request.get('/api/repos');
+//         const resRepos = JSON.parse(res.text);
         
-        expect(resRepos.length).toBe(25)
-        done();
-    });
-});
+//         expect(resRepos.length).toBe(25)
+//         done();
+//     });
+// });
 
 describe('Test POST api/repos endpoint', () => {
     it('It should respond with 201', async done => {
@@ -34,4 +34,12 @@ describe('Test POST api/repos endpoint', () => {
         expect(res.status).toBe(201);
         done();
     });
+
+    it('It should successfully save a Github user into the database', async done => {
+        const res = await request.post('/api/repos').send({ username: 'orta'});
+
+        expect(res.status).toBe(201);
+        done();
+        
+    })
 });
