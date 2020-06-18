7feee09f33a63a89c7ff500948a6f614445cc0d4
Brian Kim <briaansjkim@gmail.com>
2020-06-18 17:31:02 -0400

    completed spec suite for server api requests

--- server/server.spec.js
+++ server/server.spec.js
@@ -10,22 +10,22 @@ beforeAll(async () => {
     await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 });
 
-// describe('Test GET api/repos endpoint', () => {
-//     it('It should respond with 200', async done => {
-//         const res = await request.get('/api/repos');
+describe('Test GET api/repos endpoint', () => {
+    it('It should respond with 200', async done => {
+        const res = await request.get('/api/repos');
 
-//         expect(res.status).toBe(200);
-//         done();
-//     });
+        expect(res.status).toBe(200);
+        done();
+    });
 
-//     it('It should respond with an array of only 25 repos', async done => {
-//         const res = await request.get('/api/repos');
-//         const resRepos = JSON.parse(res.text);
+    it('It should respond with an array of only 25 repos', async done => {
+        const res = await request.get('/api/repos');
+        const resRepos = JSON.parse(res.text);
         
-//         expect(resRepos.length).toBe(25)
-//         done();
-//     });
-// });
+        expect(resRepos.length).toBe(25)
+        done();
+    });
+});
 
 describe('Test POST api/repos endpoint', () => {
     it('It should respond with 201', async done => {
